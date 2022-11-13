527d39537b6a1d20f9644433111546d14931c99a
Tony Rogvall <tony@rogvall.se>
2022-11-14 00:53:06 +0100

    added chord quality

--- tools/midi_practice.erl
+++ tools/midi_practice.erl
@@ -41,10 +41,8 @@
 	 pos = 0 :: integer(),
 	 step = 0 :: -1 | 0 | 1,
 	 map_name :: atom(),
-	 choord = "" :: string()  %% curren choord
+	 chord = "" :: string()  %% current chord
 	}).
-	 
-	 
 
 -record(state,
 	{
@@ -55,6 +53,8 @@
 	 pressure = #{} :: #{ chan() => pressure() },
 	 opts = [],
 	 match = ignore,
+	 quality = 0.0,
+	 chord :: string(), %% current chord if any
 	 seq,
 	 glyph,
 	 sharp,
@@ -114,7 +114,7 @@ init(Opts) ->
 	    ok = flush_until_select(In),
 	    Voices = lists:seq(1, 10),
 	    Lesson = proplists:get_value(lesson, Opts, {random,white_major}),
-	    Seq = #lesson{choord=Choord} = lesson_first(Lesson),
+	    Seq = #lesson{chord=_Chord} = lesson_first(Lesson),
 	    {ok,Font} = epx_font:match([{name,?FONT_NAME},{size,?FONT_SIZE}]),
 	    {W,H}  = epx_font:dimension(Font,"0"),
 	    Ascent = epx:font_info(Font, ascent),
@@ -126,7 +126,7 @@ init(Opts) ->
 	    {SW,SH}  = epx_font:dimension(SFont,"#"),
 	    SAscent = epx:font_info(SFont, ascent),
 	    Sharp = {SFont,{SW,SH},SAscent},
-	    %% io:format("PLAY: ~s\n", [Choord]),
+	    %% io:format("PLAY: ~s\n", [Chord]),
 	    {ok,IMG} = epx_image:load(filename:join(code:priv_dir(midi), 
 						    "g_clef.png")),
 	    [GClef|_] = epx_image:pixmaps(IMG),
@@ -192,10 +192,10 @@ handle_info({select,In,undefined,ready_input}, State) when
       In =:= State#state.midi_in, State#state.pending =:= 0 ->
     case midi:read(In) of
 	select -> 
-	    io:format("select: select\n", []),
+	    %% io:format("select: select\n", []),
 	    {noreply, State};
 	{ok, N} -> %% got N midi_events
-	    io:format("select: ~w new\n", [N]),
+	    %% io:format("select: ~w new\n", [N]),
 	    {noreply, State#state { pending = N} }
     end;
 handle_info({midi,In,Event}, State) when In =:= State#state.midi_in ->
@@ -261,11 +261,21 @@ draw(Pixels, _Rect, State) ->
     draw_octave(Pixels, 2),
     draw_active(Pixels, 2, State#state.active),
     draw_notes(Pixels, 2, State),
-    #lesson{choord=Choord} = State#state.seq,
+    #lesson{chord=Chord} = State#state.seq,
     {Font,_WH,Ascent} = State#state.glyph,
     epx_gc:set_font(Font),
     epx_gc:set_foreground_color(?TEXT_COLOR),
-    epx:draw_string(Pixels, 32, 64+Ascent, Choord),
+    epx:draw_string(Pixels, 32, 64+Ascent, Chord),
+    {SFont,_SWH,SAscent} = State#state.sharp,
+    epx_gc:set_font(SFont),
+    epx_gc:set_foreground_color(?TEXT_COLOR),
+    epx:draw_string(Pixels, 32, 164+SAscent, State#state.chord),
+    if State#state.match =:= false ->
+	    epx:draw_string(Pixels, 32, 184+SAscent, 
+			    "Q="++integer_to_list(trunc(State#state.quality))++"%");
+       true ->
+	    ok
+    end,
     State.
 
 %%%===================================================================
@@ -313,7 +323,7 @@ draw_active(Pixels, N, Active) ->
     X0 = 16*2,
     Y0 = 250,
     epx_gc:set_fill_color(beige),
-    maps:foreach(
+    maps_foreach(
       fun({_Chan,Note},_V) ->
 	      if Note >= ?NOTE_FIRST, Note =< ?NOTE_LAST ->
 		      Nd = Note-?NOTE_FIRST,
@@ -364,15 +374,18 @@ draw_notes(Pixels, N, #state{active=Active,
     epx_gc:set_border_width(4),
     epx_gc:set_fill_style(none),
     epx_gc:set_foreground_color(?TEXT_COLOR),
-    maps:foreach(
-      fun({_Chan,Note},_V) ->
+    maps_foreach(
+      fun({_Chan,Note},_) ->
 	      if Note >= ?NOTE_FIRST, Note =< ?NOTE_LAST ->
 		      Nd = Note-?NOTE_FIRST,
 		      Ni = Nd div 12,
 		      {K,Sharp} = note_to_sharp(Nd),
 		      I = Ni*7 + K,
 		      Y = Yoffs + (5*Step - I*Step/2 - ?NOTE_WIDTH/2),
-		      X = Xoffs + 80 + 2*?NOTE_WIDTH,
+		      X = Xoffs + 80 + 2*?NOTE_WIDTH + 
+			  if Sharp -> ?NOTE_WIDTH;
+			     true -> 0
+			  end,
 		      epx:draw_ellipse(Pixels, X, Y, 
 				       ?NOTE_WIDTH, ?NOTE_WIDTH),
 		      if Sharp ->
@@ -384,7 +397,6 @@ draw_notes(Pixels, N, #state{active=Active,
 		      ok
 	      end
       end, Active).
-
 
 -spec note_to_sharp(N::integer) -> {Key::0..6,Sharp::boolean()}.
 note_to_sharp(N) ->
@@ -483,19 +495,24 @@ note_on(Chan,Note,_Velocity,State) ->
 		{V,Vs} ->
 		    %%io:format("~w: allocate ~w, vs:~p\n", [{Chan,Note},V,Vs]),
 		    Active0 = State#state.active,
-		    Active = Active0# { {Chan,Note} =>  V },
+		    Ts = erlang:monotonic_time(micro_seconds),
+		    Active = Active0# { {Chan,Note} =>  {V,Ts} },
 		    epxw:invalidate(),
-		    #lesson{choord=Choord} = State#state.seq,
-		    case test_active(Choord, Active) of
-			true ->
+		    #lesson{chord=Chord} = State#state.seq,
+		    case match_chord(Chord, Active) of
+			{true,Q,_} ->
 			    Seq = lesson_next(State#state.seq),
 			    State#state { match = true,
+					  quality = Q,
+					  chord = "",
 					  voices = Vs, active = Active,
 					  seq = Seq };
-			false ->
+			{false,Q,Other} ->
 			    State#state { match = false,
+					  quality = Q,
+					  chord = Other,
 					  voices = Vs, active = Active };
-			ignore -> %% no choord
+			ignore -> %% no chord
 			    State#state { match = ignore,
 					  voices = Vs, active = Active }
 		    end;
@@ -509,7 +526,7 @@ note_on(Chan,Note,_Velocity,State) ->
 
 note_off(Chan,Note,_Velocity,State) ->
     case maps:take({Chan,Note}, State#state.active) of
-	{V, Active1} ->
+	{{V,_Delta}, Active1} ->
 	    Voices = release_voice(V, State#state.voices),
 	    %%%% io:format("~w: release ~w vs: ~p\n", [{Chan,Note},V,Voices]),
 	    State#state { voices = Voices, active = Active1 };
@@ -525,63 +542,100 @@ allocate_voice([V|Vs]) ->
 allocate_voice([]) ->
     false.
 
-test_active(Match, Active) ->
-    Ns = maps:fold(fun({_Chan,Note},_V,Acc) -> [Note|Acc] end, [], Active),
-    Choord = note_info_to_choord(Ns),
-    Notes = [case note_to_note_info(N) of
-	       {_,_,4,Name} -> Name;
-	       {_,_,Oct,Name} when Oct =/= 4 -> 
-		   Name++integer_to_list(Oct)
-	   end || N <- lists:sort(Ns)],
-    io:format("~p: ~s\n", [Notes, Choord]),
-    if Choord =/= "" ->
-	    Match =:= Choord;
-       true ->
-	    ignore
+%% Test if active notes match the Chord
+%% - remove potential play along base notes;
+%%      [R,R+12|Ns] -> [R+12|Ns]
+%%      [R,R+12,R+24,...] -> [R+24|Ns]
+%% - check timing 
+%%
+match_chord(MatchChord, Active) ->
+    NTs = maps:fold(fun({_Chan,Note},{_V,Ts},Acc) -> [{Note,Ts}|Acc] end, 
+		    [], Active),
+    NTs1 = normalize_notes(NTs),
+    Ns1 = [N || {N,T} <- NTs1],
+
+    %% check the chord time diff
+    Ts1 = [T || {N,T} <- NTs1],
+    Tmin = lists:min(Ts1),
+    Tmax = lists:max(Ts1),
+    Td = ((Tmax-Tmin)/1000),  %% ok round 100 ms? 
+    Quality = 100*(1-(min(Td,1000)/1000)),  %% 0-100% (FIXME)
+    Chord = notes_to_chord(Ns1),
+    display_chord(Chord, Quality, Ns1),
+    {(Chord =:= MatchChord) andalso (Quality >= 95), Quality, Chord}.
+
+display_chord(Chord, Quality, Ns) ->
+    Notes = [case note_info(N) of
+		 {_,_,4,Name} -> Name;
+		 {_,_,Oct,Name} when Oct =/= 4 -> 
+		     Name++integer_to_list(Oct)
+	     end || N <- Ns],
+    io:format("~p: quality=~f%, ~s\n", [Notes, Quality, Chord]).
+
+
+%% sort and remove "play along" one/two base notes
+normalize_notes([]) -> [];
+normalize_notes(Rs=[_]) -> Rs;
+normalize_notes(Rs=[_,_]) -> Rs;
+normalize_notes(NTs) ->
+    [{R,_T}|Rs] = lists:keysort(1, NTs),
+    R12 = R+12,
+    R24 = R+24,
+    case Rs of
+	[{R12,_T12}|Rs1=[{R24,_T24}|Rs1]] -> Rs1;
+	[{R12,_T12}|_] -> Rs;
+	_ -> NTs
     end.
+   
+notes_to_chord([]) -> "";
+notes_to_chord(Ns) -> notes_to_chord_(Ns, 0).
 
-
-note_info_to_choord([]) -> "";
-note_info_to_choord(Ns) -> notes_to_choord(Ns, 0).
-
-notes_to_choord(Ns0, Cnt) ->
+notes_to_chord_(Ns0, Cnt) ->
     [R|Rs] = Rs1 = lists:sort(Ns0),
-    {_,_Note,_Octave,Name} = note_to_note_info(R),
+    {_,_Note,_Octave,Name} = note_info(R),
     Rd = [(Ri-R) || Ri<-Rs],
     io:format("R:~w, Rd:~w\n", [R,Rd]),
     case Rd of
+	%% triads
 	[4,7]          -> Name ++ "maj";
-	[4,7,9]        -> Name ++ "6";
-	[4,7,10]       -> Name ++ "7";
-	[4,7,9,13]     -> Name ++ "9";
-	[4,7,10,13,17] -> Name ++ "11";
-	[10,14,17,21]  -> Name ++ "13";
-	[4,7,14]       -> Name ++ "add9";
-	[4,6,10]       -> Name ++ "maj7b5";
-	[4,7,11]       -> Name ++ "maj7";
-	[4,7,11,14]    -> Name ++ "maj9";
 	[3,7]          -> Name ++ "min";
+	[3,6]          -> Name ++ "dim";
+	[4,9]          -> Name ++ "aug";
+	[5,7]          -> Name ++ "sus4";
+	[2,7]          -> Name ++ "sus2";
+
+	%% extensions
 	[3,7,10]       -> Name ++ "min7";
 	[3,7,11]       -> Name ++ "min#7";
-	[3,6]          -> Name ++ "dim";
 	[3,6,9]        -> Name ++ "dim7";
+	[4,6,10]       -> Name ++ "maj7b5";
+	[4,7,9]        -> Name ++ "6";
+	[4,7,10]       -> Name ++ "7";
+	[4,7,11]       -> Name ++ "maj7";
+	[4,7,14]       -> Name ++ "add9";  %% major 9
+
+	%% quint
+	[4,7,9,13]     -> Name ++ "9";
+	[4,7,11,14]    -> Name ++ "maj9";
+	[4,7,10,13,17] -> Name ++ "11";
+	[10,14,17,21]  -> Name ++ "13";
 	_ when Cnt =:= 0 ->
 	    case Rs1 of
 		[A,B,C|Ns]  ->
-		    case notes_to_choord([A+12,B+12,C|Ns], Cnt+1) of
+		    case notes_to_chord_([A+12,B+12,C|Ns], Cnt+1) of
 			"" ->
-			    case notes_to_choord([A+12,B,C|Ns], Cnt+1) of
+			    case notes_to_chord_([A+12,B,C|Ns], Cnt+1) of
 				"" -> "";
-				Choord -> Choord ++ " 2nd"
+				Chord -> Chord ++ " 2nd"
 			    end;
-			Choord -> Choord ++ " 1st"
+			Chord -> Chord ++ " 1st"
 		    end;
 		_ -> ""
 	    end;
 	_ -> ""
     end.
 
-note_to_note_info(N) ->
+note_info(N) ->
     Octave = (N div 12) - 1,
     Note = (N rem 12) + 1,
     Name = case Note of
@@ -613,13 +667,13 @@ lesson_next(L=#lesson{type=random,map_name=Less}) ->
     N = maps:size(Map),
     I = rand:uniform(N),
     Next = maps:get(I, Map),
-    L#lesson{pos=I,choord=Next};
+    L#lesson{pos=I,chord=Next};
 lesson_next(L=#lesson{type=seq,pos=I,step=S,map_name=Less}) ->
     Map = less(Less),
     N = maps:size(Map),
     I1 = (I+S+N) rem N,
     Next = maps:get(I1+1, Map),
-    L#lesson{pos=I1,choord=Next}.
+    L#lesson{pos=I1,chord=Next}.
 
 -define(WHITE_NOTES, ["C","D","E","F","G","A","B"]).
 -define(BLACK_NOTES, ["C#","D#","F#","G#","A#"]).
@@ -641,3 +695,13 @@ less(all) -> notes_to_map(?WHITE_MAJ++?WHITE_MIN++?BLACK_MAJ++?BLACK_MIN).
 
 notes_to_map(Ns) ->
     maps:from_list(lists:zip(lists:seq(1,length(Ns)), Ns)).
+
+%%
+maps_foreach(Fun, Map) ->
+    try maps:foreach(Fun, Map) of
+	ok -> ok
+    catch
+	error:undef ->
+	    maps:fold(fun(K,V,_) -> Fun(K,V) end, Map),
+	    ok
+    end.
